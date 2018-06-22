7691b0c9a7af134a2147b49d561f2913616eda22
Andy Haden <andyh2@me.com>
2018-06-21 20:33:12 -0700

    map draws route, marker

--- src/components/singlemap/index.js
+++ src/components/singlemap/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Obstruction from 'obstruction';
+import raf from 'raf';
+
 
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+import { LngLatBounds } from 'mapbox-gl';
 import ReactMapGL from 'react-map-gl';
+
+import TimelineWorker from '../../timeline';
+
 import RouteApi from '../../api/route';
 
 const styles = theme => {
@@ -23,46 +29,127 @@ class SingleMap extends Component {
       longitude: -122.4376,
       zoom: 8
     },
+    route: null,
+    coords: [],
+  }
+
+  constructor(props){
+    super(props);
+
+    this.initMap = this.initMap.bind(this);
+    this.posAtOffset = this.posAtOffset.bind(this);
+    this.updateMarkerPos = this.updateMarkerPos.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.segment && nextProps.segment
-        && nextProps.segment.route !== this.props.segment.route) {
-      // TODO clear map
+    let nextRoute = nextProps.segment && nextProps.segment.route;
 
+    if (nextRoute && nextRoute !== this.state.route) {
+      let map = this.map.getMap();
+      this.setState({ route: nextRoute });
 
-      // fetch coords
       RouteApi(nextProps.segment.url).getCoords().then(coords => {
         const coordsArr = coords.map(coord => [coord.lng, coord.lat]);
-        const geoJson = {
-          "id": "route",
-          "type": "line",
-          "source": {
-          "type": "geojson",
-          "data": {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                  "type": "LineString",
-                  "coordinates": coordsArr,
-              }
-            }
-          },
-          "layout": {
-            "line-join": "round",
-            "line-cap": "round"
-          },
-          "paint": {
-            "line-color": "#888",
-            "line-width": 8
+        this.setState({ coords: coordsArr });
+        map.getSource('route').setData({
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+              "type": "LineString",
+              "coordinates": coordsArr,
           }
-        };
+        });
 
-        this.map.getMap().addLayer(geoJson);
+        var bounds = coordsArr.reduce(function(bounds, coord) {
+          return bounds.extend(coord);
+        }, new LngLatBounds(coordsArr[0], coordsArr[1]));
+        map.fitBounds(bounds, { padding: 20 });
       });
     }
   }
 
+  componentDidMount() {
+    this.updateMarkerPos();
+  }
+
+  updateMarkerPos() {
+    if (this.props.segment) {
+      const { routeOffset } = this.props.segment;
+      const offset = TimelineWorker.currentOffset();
+
+      const pos = this.posAtOffset(offset - routeOffset);
+      if (pos && this.map) {
+        this.map.getMap().getSource('seekPoint').setData({
+          "type": "Point",
+          "coordinates": pos
+        });
+      }
+    }
+    raf(this.updateMarkerPos);
+  }
+
+  posAtOffset(offset) {
+    const coordsIdx = Math.floor(offset / 1e3);
+    return this.state.coords[coordsIdx];
+  }
+
+  initMap(mapComponent) {
+    if (!mapComponent) {
+      this.map = null;
+      return;
+    }
+
+    let map = mapComponent.getMap();
+    map.on('load', () => {
+      map.addSource('route', {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+              "type": "LineString",
+              "coordinates": [],
+          }
+        }
+      });
+      map.addSource('seekPoint', {
+        "type": "geojson",
+        "data": {
+          "type": "Point",
+          "coordinates": []
+        }
+      });
+
+      const lineGeoJson = {
+        "id": "routeLine",
+        "type": "line",
+        "source": "route",
+        "layout": {
+          "line-join": "round",
+          "line-cap": "round"
+        },
+        "paint": {
+          "line-color": "#888",
+          "line-width": 8
+        }
+      };
+      map.addLayer(lineGeoJson);
+
+      const markerGeoJson = {
+        "id": "marker",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        },
+        "source": "seekPoint"
+      };
+
+      map.addLayer(markerGeoJson);
+
+      this.map = mapComponent;
+    });
+  }
 
   render () {
     return (
@@ -72,7 +159,8 @@ class SingleMap extends Component {
           mapStyle={this.state.mapStyle}
           onViewportChange={(viewport) => this.setState({viewport})}
           mapboxApiAccessToken={ MAPBOX_TOKEN }
-          ref={(map) => { this.map = map; }}
+          attributionControl={ false }
+          ref={ this.initMap }
         />
       </React.Fragment>
     );
@@ -80,8 +168,10 @@ class SingleMap extends Component {
 }
 
 const stateToProps = Obstruction({
+  offset: 'workerState.offset',
   route: 'workerState.route',
-  segment: 'workerState.currentSegment'
+  segment: 'workerState.currentSegment',
+  startTime: 'workerState.startTime'
 });
 
 export default connect(stateToProps)(withStyles(styles)(SingleMap));
