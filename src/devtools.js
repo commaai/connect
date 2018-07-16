711cdfbe5c8c88e09699b953f78fa8181772295d
Andy Haden <andy@comma.ai>
2018-07-15 22:16:10 -0700

    no redux devtools in production

--- src/devtools.js
+++ src/devtools.js
@@ -2,10 +2,14 @@ import window from 'global/window';
 import { compose } from 'redux';
 import * as Actions from './actions';
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      actionCreators: Object.values(Actions).filter((f) => f.name !== 'updateState')
-    }) : compose;
+let composeEnhancers;
+
+if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    actionCreators: Object.values(Actions).filter((f) => f.name !== 'updateState')
+  });
+} else {
+  composeEnhancers = compose;
+}
 
 export default composeEnhancers;
