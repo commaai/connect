d69877fb00e9f38431281828d7b0c499a1fe480c
Andy Haden <andyh2@me.com>
2019-01-23 14:16:52 -0800

    gracefully handle exception + avoid mixing async/await & promises

--- src/api/geocode.js
+++ src/api/geocode.js
@@ -1,3 +1,5 @@
+import Raven from 'raven-js';
+
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const geocodingClient = mbxGeocoding({
   accessToken: 'pk.eyJ1IjoiY29tbWFhaSIsImEiOiJjamlud2h2czAwNTN5M3dxZWg2Z3hmNnEwIn0.aam-7k03KBbMbtR7cUJslw'
@@ -19,27 +21,29 @@ export default function geocodeApi() {
 
   return {
     reverseLookup: async function(coords) {
-      const location = await geocodingClient.reverseGeocode({
+      const response = await geocodingClient.reverseGeocode({
         query: [ coords[0], coords[1] ],
         limit: 1,
-      }).send().then(response => {
-        return new Promise((resolve, reject) => {
-          const features = response.body.features;
-          if (features[0].context) {
-            let region = features[0].context.filter(getRegion)[0].short_code;
-            if (region.includes('US-')) { region = region.substr(3); }
-            const locality = features[0].context.filter(getLocality)[0].text;
-            const _neighborhood = features[0].context.filter(getNeighborhood)[0];
-            const neighborhood = _neighborhood ? _neighborhood.text : locality
-            resolve ({
-              region,
-              locality,
-              neighborhood,
-            })
-          }
-        })
-      })
-      return location;
+      }).send();
+
+      try {
+        const features = response.body.features;
+        if (features[0].context) {
+          let region = features[0].context.filter(getRegion)[0].short_code;
+          if (region.includes('US-')) { region = region.substr(3); }
+          const locality = features[0].context.filter(getLocality)[0].text;
+          const _neighborhood = features[0].context.filter(getNeighborhood)[0];
+          const neighborhood = _neighborhood ? _neighborhood.text : locality
+
+          return {
+            region,
+            locality,
+            neighborhood,
+          };
+        }
+      } catch(err) {
+        Raven.captureException(err);
+      }
     }
   }
 
