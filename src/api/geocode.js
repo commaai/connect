ed6bc75f67eab213fd1cc1477140d3c559f9f0fb
Andy Haden <andy@comma.ai>
2019-06-12 11:04:07 -0700

    use indexOf instead of includes, which is sparsely supported

--- src/api/geocode.js
+++ src/api/geocode.js
@@ -8,15 +8,15 @@ const geocodingClient = mbxGeocoding({
 export default function geocodeApi() {
 
   function getNeighborhood (context) {
-    return (context.id.includes('neighborhood'));
+    return (context.id.indexOf('neighborhood') !== -1);
   }
 
   function getLocality (context) {
-    return (context.id.includes('place'));
+    return (context.id.indexOf('place') !== -1);
   }
 
   function getRegion (context) {
-    return (context.id.includes('region'));
+    return (context.id.indexOf('region') !== -1);
   }
 
   return {
@@ -30,7 +30,7 @@ export default function geocodeApi() {
         const features = response.body.features;
         if (features[0].context) {
           let region = features[0].context.filter(getRegion)[0].short_code;
-          if (region.includes('US-')) { region = region.substr(3); }
+          if (region.indexOf('US-') !== -1) { region = region.substr(3); }
           const locality = features[0].context.filter(getLocality)[0].text;
           const _neighborhood = features[0].context.filter(getNeighborhood)[0];
           const neighborhood = _neighborhood ? _neighborhood.text : locality
