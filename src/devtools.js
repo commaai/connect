c8f9ba0ea8774e0411ed23a643a7ac85962c3ae7
Joost Wooning <jwooning@gmail.com>
2021-05-18 14:16:08 +0200

    set up serviceworker and manifest

--- src/devtools.js
+++ src/devtools.js
@@ -2,7 +2,7 @@ import window from 'global/window';
 import { compose } from 'redux';
 
 function getComposeEnhancers() {
-  if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+  if (process.env.REACT_APP_DEVTOOLS && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
     return window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
   }
   return compose;
