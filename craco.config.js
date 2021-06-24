c0f1d215f873e0e209dd186b43b33f7d42af8c47
Joost Wooning <jwooning@gmail.com>
2021-06-24 12:36:10 +0200

    cleanup craco

--- craco.config.js
+++ craco.config.js
@@ -1,42 +1,8 @@
-/* eslint-disable */
 const { removeLoaders, loaderByName, addBeforeLoader } = require('@craco/craco');
-const SentryPlugin = require('craco-sentry-plugin');
+const SentryCliPlugin = require('@sentry/webpack-plugin');
 
 module.exports = function ({ env }) {
-  const plugins = [{
-    plugin: {
-      overrideWebpackConfig: ({ pluginOptions, webpackConfig, context: { env } }) => {
-        const workerLoader = {
-          test: /\.worker\.js/,
-          use: {
-            loader: "worker-loader",
-            options: pluginOptions || {}
-          }
-        };
-        addBeforeLoader(webpackConfig, loaderByName("babel-loader"), workerLoader);
-        return webpackConfig;
-      }
-    }
-  }];
-  if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_ENV) {
-    plugins.push({
-      plugin: {
-        overrideWebpackConfig: ({ pluginOptions, webpackConfig, context: { env } }) => {
-          const sentryPlugin = new SentryCliPlugin({
-            include: './build/',
-            ignoreFile: '.sentrycliignore',
-            ignore: ['node_modules', 'webpack.config.js', 'craco.config.js'],
-            configFile: 'sentry.properties',
-            ...pluginOptions
-          });
-          webpackConfig.plugins.push(sentryPlugin);
-          return webpackConfig;
-        }
-      }
-    });
-  }
   return {
-    plugins,
     jest: {
       configure: (jestConfig, { env, paths }) => {
         jestConfig.testPathIgnorePatterns = ['node_modules', '__puppeteer__'];
@@ -45,7 +11,19 @@ module.exports = function ({ env }) {
     },
     webpack: {
       configure: (webpackConfig, { env, paths }) => {
+        if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_ENV) {
+          webpackConfig.plugins.push(new SentryCliPlugin({
+            include: './build/',
+            ignoreFile: '.sentrycliignore',
+            ignore: ['node_modules', 'webpack.config.js', 'craco.config.js'],
+            configFile: 'sentry.properties',
+          }))
+        }
         webpackConfig.output.globalObject = 'this';
+        addBeforeLoader(webpackConfig, loaderByName("babel-loader"), {
+          test: /\.worker\.js/,
+          use: { loader: "worker-loader" }
+        });
         removeLoaders(webpackConfig, loaderByName('eslint-loader'));
         webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.map(function (plugin) {
           if (plugin.constructor.name !== 'TerserPlugin') {
