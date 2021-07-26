a56a9eb5429441e2cb75ced66a79aabe81615be4
Joost Wooning <jwooning@gmail.com>
2021-07-26 14:06:22 +0200

    fix tests

--- src/timeline/index.test.js
+++ src/timeline/index.test.js
@@ -3,10 +3,7 @@ import { TimelineInterface } from './index';
 import { pause } from './playback';
 import store from './store';
 
-const LogReaderWorker = require('./logReader');
-
 jest.mock('./startup');
-jest.mock('./logReader', () => jest.fn());
 jest.mock('./timeline', () => ({
   init: jest.fn(() => new Promise((resolve) => resolve())),
   getState: jest.fn(() => {
@@ -20,14 +17,8 @@ jest.mock('./commands', () => ({
     cachePort: jest.fn(() => new Promise((resolve) => resolve())),
   }
 }));
-LogReaderWorker.onData = jest.fn();
 
 describe('timeline index', () => {
-  beforeAll(() => {
-    LogReaderWorker.mockClear();
-    LogReaderWorker.onData.mockClear();
-  });
-
   it('fails without auth', async () => {
     const timeline = new TimelineInterface();
     expect(timeline).toBeTruthy();
@@ -53,25 +44,5 @@ describe('timeline index', () => {
 
     store.dispatch(pause());
     expect(stateHandler).toBeCalled();
-  });
-
-  it('listens for log reader events', async () => {
-    const timeline = new TimelineInterface();
-
-    await timeline.init(true);
-
-    expect(LogReaderWorker.onData).toBeCalled();
-
-    const indexHandler = jest.fn();
-    timeline.onIndexed(indexHandler);
-
-    LogReaderWorker.onData.mock.calls[0][0]({
-      data: {
-        route: 'asdffdsaasd',
-        segment: 1,
-        data: []
-      }
-    });
-    expect(indexHandler).toBeCalled();
   });
 });
