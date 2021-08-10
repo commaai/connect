1c9976db7af576d95ebe035d2d50b3d437f88034
Joost Wooning <jwooning@gmail.com>
2021-08-10 17:32:16 +0200

    set urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,12 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    collect: {
+      url: [
+        'http://localhost/',
+        'http://localhost/?demo=1',
+        'http://localhost/3533c53bb29502d1/1575969207023/1575976457225/?demo=1',
+      ],
+    },
   },
 };
