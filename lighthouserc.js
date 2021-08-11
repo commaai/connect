ae60d214faa03da7d3d6af3062795ac17da5d140
Joost Wooning <jwooning@gmail.com>
2021-08-11 11:18:36 +0200

    server port

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,11 +4,11 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     collect: {
-      startServerCommand: 'yarn serve -s -C build',
+      startServerCommand: 'yarn serve -s -C -l 3000 build',
       url: [
-        'http://localhost/',
-        'http://localhost/?demo=1',
-        'http://localhost/3533c53bb29502d1/1575969207023/1575976457225/?demo=1',
+        'http://localhost:3000/',
+        'http://localhost:3000/?demo=1',
+        'http://localhost:3000/3533c53bb29502d1/1575969207023/1575976457225/?demo=1',
       ],
     },
   },
