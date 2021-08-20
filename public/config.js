3137a8fb695f3ae700d78793f8065381206c7853
Joost Wooning <jwooning@gmail.com>
2021-08-20 11:41:49 +0200

    update config

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.COMMA_URL_ROOT = 'https://api.commadotai.com/';
 window.ATHENA_URL_ROOT = 'https://athena.comma.ai/';
 window.BILLING_URL_ROOT = 'https://billing.comma.ai/';
-window.VIDEO_HOST = 'https://my-comma-video.azureedge.net';
+window.VIDEO_HOST = 'https://my-comma-video-development.azureedge.net';
