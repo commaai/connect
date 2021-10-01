ab08eeda7fefd234d62a6175f60676c0e26fd9b1
Joost Wooning <jwooning@gmail.com>
2021-10-01 16:59:03 +0200

    use staticdistdir

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,11 +4,12 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     collect: {
-      startServerCommand: 'yarn serve -s -C -l 3000 build',
+      staticDistDir: 'build',
+      isSinglePageApplication: true,
       url: [
-        'http://localhost:3000/',
-        'http://localhost:3000/3533c53bb29502d1',
-        'http://localhost:3000/3533c53bb29502d1/1575969207023/1575976457225/',
+        'http://localhost/',
+        'http://localhost/3533c53bb29502d1',
+        'http://localhost/3533c53bb29502d1/1575969207023/1575976457225/',
       ],
     },
   },
