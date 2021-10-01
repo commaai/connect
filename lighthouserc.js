e884ff650e98417fafb05cd84388e685ca7b4a91
Joost Wooning <jwooning@gmail.com>
2021-10-01 16:40:51 +0200

    update urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,8 +7,8 @@ module.exports = {
       startServerCommand: 'yarn serve -s -C -l 3000 build',
       url: [
         'http://localhost:3000/',
-        'http://localhost:3000/?demo=1',
-        'http://localhost:3000/3533c53bb29502d1/1575969207023/1575976457225/?demo=1',
+        'http://localhost:3000/3533c53bb29502d1',
+        'http://localhost:3000/3533c53bb29502d1/1575969207023/1575976457225/',
       ],
     },
   },
