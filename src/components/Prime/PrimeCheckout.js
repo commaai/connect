15c601b9eb737a1f0c7df68e5354d3dba2ee702a
Joost Wooning <jwooning@gmail.com>
2021-11-16 16:02:40 +0100

    first design try

--- src/components/Prime/PrimeCheckout.js
+++ src/components/Prime/PrimeCheckout.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux';
 import Obstruction from 'obstruction';
 import fecha from 'fecha';
 import * as Sentry from '@sentry/react';
-import { withStyles, Typography, IconButton, Button, CircularProgress } from '@material-ui/core';
+import { withStyles, Typography, IconButton, Button, CircularProgress, List, ListItem, ListItemIcon, ListItemText
+  } from '@material-ui/core';
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 import ErrorIcon from '@material-ui/icons/ErrorOutline';
 import WarningIcon from '@material-ui/icons/Warning';
+import CheckIcon from '@material-ui/icons/Check';
 
 import { billing as Billing } from '@commaai/comma-api';
 
 import { deviceTypePretty } from '../../utils';
-import PrimeChecklist from './PrimeChecklist';
 import ResizeHandler from '../ResizeHandler';
 import Colors from '../../colors';
 import { primeNav } from '../../actions';
@@ -41,10 +42,6 @@ const styles = (theme) => ({
   moreInfoContainer: {
     '& p': { display: 'inline' },
     '& button': { display: 'inline', marginLeft: '15px' },
-  },
-  introLine: {
-    fontWeight: 500,
-    lineHeight: '23px',
   },
   checkList: {
     marginLeft: 10,
@@ -96,12 +93,12 @@ const styles = (theme) => ({
     fontSize: 13,
   },
   buttons: {
-    height: 48,
+    height: 32,
+    width: '100%',
     background: Colors.white,
-    borderRadius: 24,
+    borderRadius: 18,
     color: '#404B4F',
     textTransform: 'none',
-    width: 200,
     '&:hover': {
       backgroundColor: Colors.white70,
       color: '#404B4F',
@@ -114,6 +111,49 @@ const styles = (theme) => ({
       backgroundColor: Colors.white70,
       color: '#404B4F',
     }
+  },
+  checkList: {
+    marginLeft: 24,
+    '& span': { fontSize: 14 },
+  },
+  checkListItem: {
+    padding: 0,
+    '& svg': {
+      alignSelf: 'flex-start',
+      fontSize: 21,
+      marginRight: 0,
+    },
+  },
+  learnMore: {
+    '& a': { color: 'white' },
+  },
+  plan: {
+    '& > div': {
+      borderLeft: `1px solid ${Colors.white50}`,
+      borderRight: `1px solid ${Colors.white50}`,
+      '&:first-child': {
+        borderRadius: '18px 18px 0 0',
+        borderTop: `1px solid ${Colors.white50}`,
+      },
+      '&:last-child': {
+        paddingBottom: 8,
+        borderRadius: '0 0 18px 18px',
+        borderBottom: `1px solid ${Colors.white50}`,
+      },
+    },
+  },
+  planHeaderBox: {
+    paddingTop: 12,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  planHeader: {
+    fontWeight: 600,
+    '& span': {
+      fontWeight: 'normal',
+      fontSize: '0.8rem',
+    },
   },
 });
 
@@ -154,10 +194,11 @@ class PrimeCheckout extends Component {
     }
   }
 
-  async gotoCheckout() {
+  async gotoCheckout(plan) {
+    const { dongleId, subscribeInfo } =  this.props;
     this.setState({ loadingCheckout: true });
     try {
-      const resp = await Billing.getStripeCheckout(this.props.dongleId, this.props.subscribeInfo.sim_id);
+      const resp = await Billing.getStripeCheckout(dongleId, subscribeInfo.sim_id, plan);
       window.location = resp.url;
     } catch (err) {
       // TODO show error messages
@@ -168,7 +209,19 @@ class PrimeCheckout extends Component {
 
   render() {
     const { classes, device, subscribeInfo } = this.props;
-    const { windowWidth, error, loadingCheckout } = this.state;
+    const { windowWidth, error, loadingCheckout, selectedPlan } = this.state;
+
+    const listItems = [
+      ['On-device navigation', null],
+      ['1 year storage of drive videos', null],
+      ['Simple SSH for developers', null],
+    ];
+
+    const listItems2 = [
+      ['All basic comma prime perks', null],
+      ['24/7 connectivity', null],
+      ['Unlimited data at 512kbps', 'only offered in the United States'],
+    ]
 
     let chargeText = null;
     if (subscribeInfo) {
@@ -184,7 +237,7 @@ class PrimeCheckout extends Component {
 
     const alias = device.alias || deviceTypePretty(device.device_type);
     const containerPadding = windowWidth > 520 ? '24px 36px 36px' : '2px 12px 12px';
-    const buttonSmallStyle = windowWidth < 514 ? { width: '100%' } : {};
+    const paddingStyle = windowWidth > 520 ? { paddingLeft: 7, paddingRight: 7 } : { paddingLeft: 8, paddingRight: 8 };
 
     return ( <>
       <div className={ classes.primeBox } style={{ padding: containerPadding }}>
@@ -198,8 +251,68 @@ class PrimeCheckout extends Component {
             <Typography variant="caption" className={classes.deviceId}>({ device.dongle_id })</Typography>
           </div>
         </div>
-        <Typography className={ classes.introLine }>Become a comma prime member today for only $24/month</Typography>
-        <PrimeChecklist />
+        <div className={ classes.overviewBlock }>
+          <div className={ classes.plan }>
+            <div className={ classes.planHeaderBox }>
+              <div className={ classes.planHeader } style={ paddingStyle }>
+                basic comma prime ($16/month)<br />
+                <span>internet connection required</span>
+              </div>
+            </div>
+            <div style={ paddingStyle }>
+              <List className={ classes.checkList }>
+                { listItems.map((listItemText, i) => {
+                  return <ListItem key={ i } className={ classes.checkListItem } style={ paddingStyle }>
+                    <ListItemIcon><CheckIcon /></ListItemIcon>
+                    <ListItemText primary={ listItemText[0] } secondary={ listItemText[1] } />
+                  </ListItem>;
+                }) }
+              </List>
+            </div>
+            <div style={ paddingStyle }>
+              <Button className={ `${classes.buttons} gotoCheckout` } onClick={ () => this.gotoCheckout('nodata') }
+                disabled={ Boolean(!subscribeInfo || !subscribeInfo.sim_id || loadingCheckout) }>
+                { loadingCheckout ?
+                  <CircularProgress size={ 19 } /> :
+                  ((subscribeInfo && subscribeInfo.trial_claimable) ? 'Claim trial' : 'Go to checkout')
+                }
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={ classes.overviewBlock }>
+          <div className={ classes.plan }>
+            <div className={ classes.planHeaderBox }>
+              <div className={ classes.planHeader } style={ paddingStyle }>
+                unlimited comma prime ($24/month)
+              </div>
+            </div>
+            <div style={ paddingStyle }>
+              <List className={ classes.checkList }>
+                { listItems2.map((listItemText, i) => {
+                  return <ListItem key={ i } className={ classes.checkListItem } style={ paddingStyle }>
+                    <ListItemIcon><CheckIcon /></ListItemIcon>
+                    <ListItemText primary={ listItemText[0] } secondary={ listItemText[1] } />
+                  </ListItem>;
+                }) }
+              </List>
+            </div>
+            <div style={ paddingStyle }>
+              <Button className={ `${classes.buttons} gotoCheckout` } onClick={ () => this.gotoCheckout('nodata') }
+                disabled={ Boolean(!subscribeInfo || !subscribeInfo.sim_id || loadingCheckout) }>
+                { loadingCheckout ?
+                  <CircularProgress size={ 19 } /> :
+                  ((subscribeInfo && subscribeInfo.trial_claimable) ? 'Claim trial' : 'Go to checkout')
+                }
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={ classes.overviewBlock }>
+          <Typography className={ classes.learnMore }>
+            Learn more about comma prime from our <a rel="noreferrer" target="_blank" href="https://comma.ai/prime#faq">FAQ</a>
+          </Typography>
+        </div>
         { error && <div className={ classes.overviewBlockError }>
           <ErrorIcon />
           <Typography>{ error }</Typography>
@@ -224,20 +337,6 @@ class PrimeCheckout extends Component {
             <Typography>
               Third-party SIM detected, comma prime can be activated, but no data connection will be provided.
             </Typography>
-          </div>
-        }
-        <div className={ classes.overviewBlock }>
-          <Button className={ `${classes.buttons} gotoCheckout` } style={ buttonSmallStyle } onClick={ this.gotoCheckout }
-            disabled={ Boolean(!subscribeInfo || !subscribeInfo.sim_id || loadingCheckout) }>
-            { loadingCheckout ?
-              <CircularProgress size={ 19 } /> :
-              ((subscribeInfo && subscribeInfo.trial_claimable) ? 'Claim trial' : 'Go to checkout')
-            }
-          </Button>
-        </div>
-        { chargeText &&
-          <div className={ classes.overviewBlock }>
-            <Typography className={ classes.chargeText }>{ chargeText }</Typography>
           </div>
         }
       </div>
