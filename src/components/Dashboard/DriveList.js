ee65ad2d17fe4909c27985971a1f3cb8be03379c
Joost Wooning <jwooning@gmail.com>
2021-12-06 15:44:55 +0100

    fix drivelist order

--- src/components/Dashboard/DriveList.js
+++ src/components/Dashboard/DriveList.js
@@ -77,7 +77,7 @@ class DriveList extends Component {
   render() {
     const { classes, dongleId } = this.props;
 
-    const driveList = this.props.segments.reverse().map((segment) => ({
+    const driveList = Object.assign([], this.props.segments).reverse().map((segment) => ({
       ...segment,
       dongleId: dongleId,
     }));
