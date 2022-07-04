accaecfcbe06bc1079045ad444ea3f928c2edc42
Joost Wooning <jwooning@gmail.com>
2022-07-04 16:45:50 +0200

    some desktop styles

--- src/components/ClipView/ClipList.js
+++ src/components/ClipView/ClipList.js
@@ -19,31 +19,24 @@ const styles = (theme) => ({
   clipItemHeader: {
     display: 'flex',
     alignItems: 'center',
-    padding: 3,
     color: Colors.white,
     '& h6': {
       padding: '0 3px',
-      fontSize: '0.8rem',
       margin: 0,
     },
   },
   clipItem: {
     display: 'flex',
     alignItems: 'center',
-    padding: 3,
     color: Colors.white,
     borderTop: '1px solid rgba(255, 255, 255, .05)',
     textDecoration: 'none',
     '&:hover': {},
     '& p': {
       padding: '0 3px',
-      fontSize: '0.8rem',
       margin: 0,
       textAlign: 'center',
     },
-  },
-  clipPublicIcon: {
-    fontSize: '1rem',
   },
   clipTitle: {
     'p&': {
@@ -51,7 +44,6 @@ const styles = (theme) => ({
     }
   },
   clipPlayIcon: {
-    fontSize: '1.2rem',
     paddingRight: 3,
   },
 });
@@ -73,8 +65,9 @@ class ClipList extends Component {
   }
 
   render() {
-    const { classes, device, clips } = this.props;
+    const { classes, clips } = this.props;
     const { windowWidth } = this.state;
+
     const viewerPadding = windowWidth < 768 ? 12 : 32;
 
     const gridWidths = windowWidth < 768 ?
@@ -82,18 +75,20 @@ class ClipList extends Component {
       [7, 66, 20, 7];
     const gridStyles = gridWidths.map((w) => ({ maxWidth: `${w}%`, flexBasis: `${w}%` }));
 
+    const itemStyle = windowWidth < 768 ? { fontSize: '0.8rem' } : { fontSize: '1rem' };
+
     return <>
       <ResizeHandler onResize={ this.onResize } />
 
-      <div style={{ padding: viewerPadding }}>
+      <div style={{ ...itemStyle, padding: viewerPadding }}>
         { !clips.list && <CircularProgress style={{ margin: 12, color: Colors.white }} size={ 20 } /> }
         { Boolean(clips.list && clips.list.length === 0) && <p>no clips found</p> }
         { Boolean(clips.list && clips.list.length > 0) &&
-          <div className={classes.clipItemHeader}>
-            <h6 style={ gridStyles[0] }></h6>
-            <h6 style={ gridStyles[1] }>title</h6>
-            <h6 style={{ ...gridStyles[2], textAlign: 'center' }}>date</h6>
-            <h6 style={ gridStyles[3] }></h6>
+          <div className={classes.clipItemHeader} style={{ padding: (windowWidth < 768 ? 3 : 8) }}>
+            <h6 style={{ ...itemStyle, ...gridStyles[0] }}></h6>
+            <h6 style={{ ...itemStyle, ...gridStyles[1] }}>title</h6>
+            <h6 style={{ ...itemStyle, ...gridStyles[2], textAlign: 'center' }}>date</h6>
+            <h6 style={{ ...itemStyle, ...gridStyles[3] }}></h6>
           </div>
         }
         { clips.list && clips.list.map((c) => this.renderClipItem(gridStyles, c)) }
@@ -103,6 +98,9 @@ class ClipList extends Component {
 
   renderClipItem(gridStyles, c) {
     const { classes, dongleId } = this.props;
+    const { windowWidth } = this.state;
+
+    const itemStyle = windowWidth < 768 ? { fontSize: '0.8rem' } : { fontSize: '1.0rem' };
 
     const timeStr = fecha.format(new Date(c.start_time), 'MMM\u00a0D h:mm\u00a0a').toLowerCase();
     const StateIconType = c.status === 'pending' ?
@@ -116,21 +114,23 @@ class ClipList extends Component {
     }
 
     const innerItem = <>
-      <StateIconType style={ firstGridItemStyle } className={ classes.clipPlayIcon } />
-      <p style={ gridStyles[1] } className={ classes.clipTitle }>{ c.title }</p>
-      <p style={ gridStyles[2] }>{ timeStr }</p>
-      <IsPublicIconType style={ gridStyles[3] } className={ classes.clipPublicIcon } />
+      <StateIconType className={ classes.clipPlayIcon }
+        style={{ ...firstGridItemStyle, fontSize: (windowWidth < 768 ? '1.2rem' : '1.4rem') }} />
+      <p style={{ ...itemStyle, ...gridStyles[1] }} className={ classes.clipTitle }>{ c.title }</p>
+      <p style={{ ...itemStyle, ...gridStyles[2] }}>{ timeStr }</p>
+      <IsPublicIconType style={{ ...gridStyles[3], fontSize: (windowWidth < 768 ? '1.0rem' : '1.2rem') }} />
     </>;
 
     if (c.status === 'failed') {
-      return <div key={c.id} className={classes.clipItem}>
+      return <div key={c.id} className={classes.clipItem} style={{ padding: (windowWidth < 768 ? 3 : 8) }}>
         { innerItem }
       </div>;
     }
 
     return (
       <a key={c.id} className={classes.clipItem} href={ `/${dongleId}/clips/${c.id}` }
-        onClick={ filterRegularClick(() => this.props.dispatch(navToClips(c.id, c.state))) } >
+        onClick={ filterRegularClick(() => this.props.dispatch(navToClips(c.id, c.state))) }
+        style={{ padding: (windowWidth < 768 ? 3 : 8) }}>
         { innerItem }
       </a>
     );
@@ -139,7 +139,6 @@ class ClipList extends Component {
 
 const stateToProps = Obstruction({
   dongleId: 'dongleId',
-  device: 'device',
   clips: 'clips',
 });
 
