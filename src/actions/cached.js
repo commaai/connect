f9e8065c988a9608ec64e11b1e8ea3366a2b6c2f
Cameron Clough <cameronjclough@gmail.com>
2022-08-23 15:04:21 -0700

    don't allow overlapping flags in timeline

--- src/actions/cached.js
+++ src/actions/cached.js
@@ -208,6 +208,10 @@ function parseEvents(route, driveEvents) {
     } else if (ev.type === 'event') {
       res.push(ev);
     } else if (ev.type === 'user_flag') {
+      if (currFlag && currFlag.data.end_route_offset_millis > ev.route_offset_millis) {
+        ev.route_offset_millis = currFlag.data.end_route_offset_millis;
+      }
+
       currFlag = {
         ...ev,
         data: {
