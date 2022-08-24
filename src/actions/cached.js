2c554bb3755eca30e878c5fdc13f4721f8293fc0
Cameron Clough <cameronjclough@gmail.com>
2022-08-24 15:44:39 -0700

    add env flag to use local chffr data server

--- src/actions/cached.js
+++ src/actions/cached.js
@@ -3,6 +3,8 @@ import * as Sentry from '@sentry/react';
 import * as Types from './types';
 import GeocodeApi from '../api/geocode';
 
+const USE_LOCAL_CHFFR_DATA = !!process.env.REACT_APP_LOCAL_CHFFR_DATA;
+
 const eventsRequests = {};
 const coordsRequests = {};
 const driveCoordsRequests = {};
@@ -294,7 +296,11 @@ export function fetchEvents(route) {
     const promises = [];
     for (let i = 0; i <= route.maxqlog; i++) {
       promises.push((async (i) => {
-        const resp = await fetch(`${route.url}/${i}/events.json`, { method: 'GET' });
+        const url = new URL(`${route.url}/${i}/events.json`);
+        if (USE_LOCAL_CHFFR_DATA) {
+          url.hostname = 'chffrprivate.azureedge.local';
+        }
+        const resp = await fetch(url, { method: 'GET' });
         if (!resp.ok) {
           return [];
         }
