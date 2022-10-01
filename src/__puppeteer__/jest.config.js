b3ea1c5e4a402ab583a9afcf941e2e4e9cef814a
Cameron Clough <cameronjclough@gmail.com>
2022-10-01 15:53:45 -0700

    craco and jest config cleanup

--- src/__puppeteer__/jest.config.js
+++ src/__puppeteer__/jest.config.js
@@ -6,7 +6,7 @@ const jestConfig = createJestConfig(cracoConfig({ env: process.env.NODE_ENV }));
 module.exports = {
   ...jestConfig,
   preset: 'jest-puppeteer',
-  testMatch: ['<rootDir>/src/__puppeteer__/**/*.test.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: [...jestConfig.setupFilesAfterEnv, 'expect-puppeteer'],
-  testPathIgnorePatterns: ['node_modules']
+  testMatch: ['<rootDir>/src/__puppeteer__/**/*.test.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: ['node_modules'],
 };
