4ce1d5099d985c2fd7f314a13a70ee8f60c0fb6a
Cameron Clough <cameronjclough@gmail.com>
2022-11-01 13:27:26 -0700

    remove unused import

--- src/components/ClipView/ClipList.js
+++ src/components/ClipView/ClipList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Obstruction from 'obstruction';
 import fecha from 'fecha';
 import * as Sentry from '@sentry/react';
 
