b3f1b116876135a49a29741939eeab30a0e6005b
Cameron Clough <cameronjclough@gmail.com>
2022-11-07 15:09:10 -0800

    geocode: simple tests

---
+++ src/api/geocode.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+import { priorityGetContext, reverseLookup, forwardLookup } from './geocode';
+
+describe('priorityGetContext', () => {
+  it('should return the first context with a priority', () => {
+    const contexts = [
+      { id: 'place.123' },
+      { id: 'locality.123' },
+      { id: 'district.123' },
+    ];
+    expect(priorityGetContext(contexts)).toEqual(contexts[0]);
+  });
+});
+
+describe('reverseLookup', () => {
+  jest.setTimeout(10000);
+
+  it('should return null if coords are [0, 0]', async () => {
+    const result = await reverseLookup([0, 0]);
+    expect(result).toBeNull();
+  });
+
+  it('should return market street', async () => {
+    const result = await reverseLookup([-117.12547, 32.71137], true);
+    expect(result).toEqual({
+      details: 'San Diego, CA 92102, United States',
+      place: 'Market Street',
+    });
+  });
+});
+
+describe('forwardLookup', () => {
+  jest.setTimeout(10000);
+
+  it('should return null if query is empty', async () => {
+    const result = await forwardLookup('');
+    expect(result).toHaveLength(0);
+  });
+
+  it('should return taco bell', async () => {
+    const result = await forwardLookup('Taco Bell, Market St, San Diego, CA');
+    expect(result[1].position).toEqual({
+      lat: 32.71155,
+      lng: -117.11647,
+    });
+  });
+});
