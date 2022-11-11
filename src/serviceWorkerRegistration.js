6b2b8bab7bbaebae6c202509b120431891ea7f24
Cameron Clough <cameronjclough@gmail.com>
2022-11-11 15:42:01 -0800

    fix update check

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -44,15 +44,6 @@ export function register(config) {
               + 'worker. To learn more, visit https://cra.link/PWA',
           );
         });
-
-        // Check for updates every minute.
-        setInterval(async () => {
-          navigator.serviceWorker.getRegistrations().then((registrations) => {
-            registrations.forEach((registration) => {
-              registration.update();
-            });
-          });
-        }, 60 * 1000);
       } else {
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
@@ -65,36 +56,44 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      const interval = setInterval(() => {
+        if (!registration) {
+          clearInterval(interval);
+          return;
+        }
+
+        console.log('Checking for updates...');
+        registration.update();
+      }, 60 * 1000);
+
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
           return;
         }
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all '
-                  + 'tabs for this page are closed. See https://cra.link/PWA.',
-              );
+          if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
+            // At this point, the updated precached content has been fetched,
+            // but the previous service worker will still serve the older
+            // content until all client tabs are closed.
+            console.log(
+              'New content is available and will be used when all '
+                + 'tabs for this page are closed. See https://cra.link/PWA.',
+            );
 
-              // Execute callback
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
-              }
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+            // Execute callback
+            if (config && config.onUpdate) {
+              config.onUpdate(registration);
+            }
+          } else {
+            // At this point, everything has been precached.
+            // It's the perfect time to display a
+            // "Content is cached for offline use." message.
+            console.log('Content is cached for offline use.');
 
-              // Execute callback
-              if (config && config.onSuccess) {
-                config.onSuccess(registration);
-              }
+            // Execute callback
+            if (config && config.onSuccess) {
+              config.onSuccess(registration);
             }
           }
         };
