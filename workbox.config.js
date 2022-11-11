c1ab1a33134bdf3a47e4cb647c93b20ffd918b1e
Cameron Clough <cameronjclough@gmail.com>
2022-11-11 15:43:11 -0800

    debug workbox

--- workbox.config.js
+++ workbox.config.js
@@ -4,11 +4,13 @@ module.exports = {
     // override GenerateSW config here
     // e.g. options.skipWaiting = true;
     options.skipWaiting = false;
+    console.log('[Workbox]', 'GenerateSW', options);
     return options;
   },
   InjectManifest: (options) => {
     // override InjectManifest config here
     // e.g. options.maximumFileSizeToCacheInBytes = 10 * 1024 * 1024;
+    console.log('[Workbox]', 'InjectManifest', options);
     return options;
   },
 };
