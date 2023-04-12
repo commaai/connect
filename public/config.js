189b0786c34e2bbd82560d9c8ce8d4e39cc2901d
Cameron Clough <cameronjclough@gmail.com>
2023-04-12 13:43:12 -0700

    remove web cabana

--- public/config.js
+++ public/config.js
@@ -2,4 +2,3 @@ window.COMMA_URL_ROOT = 'https://api.comma.ai/';
 window.ATHENA_URL_ROOT = 'https://athena.comma.ai/';
 window.BILLING_URL_ROOT = 'https://billing.comma.ai/';
 window.USERADMIN_URL_ROOT = 'https://useradmin.comma.ai/';
-window.CABANA_URL_ROOT = 'https://cabana.comma.ai/';
