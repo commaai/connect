35a72bfce72cac875d863f21a84ed5e1ab42a652
Cameron Clough <cameronjclough@gmail.com>
2023-05-18 22:50:45 -0700

    web vitals: batch analytics events

--- src/analytics-v2.js
+++ src/analytics-v2.js
@@ -2,21 +2,48 @@ import { onCLS, onFID, onLCP } from 'web-vitals';
 
 import { getCommaAccessToken } from '@commaai/my-comma-auth/storage';
 
-async function reportEvent(event) {
+const queue = new Set();
+
+let counter = 0;
+
+function uniqueId() {
+  counter += 1;
+  return `${Date.now()}-${Math.random().toString(10).substring(2, 10)}-${counter}`;
+}
+
+async function flushQueue() {
+  if (queue.size === 0) return;
+
+  // TODO: flush queue when auth state changes
   const accessToken = await getCommaAccessToken();
+
+  const body = JSON.stringify([...queue]);
+
   await fetch(`${window.COMMA_URL_ROOT}_/ping`, {
-    method: 'POST',
-    credentials: 'include',
+    body,
     headers: {
       Authorization: `JWT ${accessToken}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(event),
+    keepalive: true,
+    method: 'POST',
+  });
+
+  queue.clear();
+}
+
+export function sendEvent(event) {
+  if (!event.event) {
+    throw new Error('Analytics event must have an event property');
+  }
+  queue.add({
+    _id: uniqueId(),
+    ...event,
   });
 }
 
-async function sendToAnalytics(metric) {
-  await reportEvent({
+function sendToAnalytics(metric) {
+  sendEvent({
     event: 'web_vitals',
     ...metric,
   });
@@ -25,3 +52,15 @@ async function sendToAnalytics(metric) {
 onCLS(sendToAnalytics);
 onFID(sendToAnalytics);
 onLCP(sendToAnalytics);
+
+// Report all available metrics whenever the page is backgrounded or unloaded.
+window.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') {
+    flushQueue();
+  }
+});
+
+// NOTE: Safari does not reliably fire the `visibilitychange` event when the
+// page is being unloaded. If Safari support is needed, you should also flush the
+// queue in the `pagehide` event.
+window.addEventListener('pagehide', flushQueue);
