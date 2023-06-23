55e29ff711fb4b33bdd1966ebcf95ca62f21805b
Cameron Clough <cameronjclough@gmail.com>
2023-06-23 18:18:27 +0100

    act

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
-  it('should not crash', () => {
-    render(<App/>);
+  it('should not crash', async () => {
+    await act(() => {
+      render(<App/>);
+    });
   });
 });
