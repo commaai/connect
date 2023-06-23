d77967904b15e7931ce3d6bb463159bfdf0eec51
Cameron Clough <cameronjclough@gmail.com>
2023-06-23 17:56:11 +0100

    progress

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ const config = {
     '!src/mocks/**',
   ],
   coveragePathIgnorePatterns: [],
-  setupFilesAfterEnv: [],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['node_modules', 'src/__puppeteer__'],
   transform: {
@@ -16,30 +15,14 @@ const config = {
     '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
   },
   transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    // '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
     '^.+\\.module\\.(css|sass|scss)$',
-    // 'node_modules/(?!(.*@commaai.*)/)',
+    'node_modules/(?!(.*@commaai.*)/)',
   ],
   modulePaths: ['<rootDir>/src'],
   moduleNameMapper: {
-    // '^react-native$': 'react-native-web',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
-  moduleFileExtensions: [
-    // Place tsx and ts to beginning as suggestion from Jest team
-    // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
-    'tsx',
-    'ts',
-    'web.js',
-    'js',
-    'web.ts',
-    'web.tsx',
-    'json',
-    'web.jsx',
-    'jsx',
-    'node',
-  ],
-  resetMocks: true,
 };
 
 module.exports = config;
--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,10 +1,9 @@
-/* eslint-env jest */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('App', () => {
   it('should work as expected', () => {
-    // render(<App/>);
-    expect(1 + 1).toBe(2);
+    render(<App/>);
+    expect(screen.getByText("comma connect")).toBeInTheDocument();
   });
 });
