a3a2224c8d414deb20a39d48dac45f0bb1ed90ff
Cameron Clough <cameronjclough@gmail.com>
2023-06-23 18:23:59 +0100

    clear the mocks

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,2 +1,15 @@
+/* eslint-env jest */
 import '@testing-library/jest-dom';
 import 'whatwg-fetch';
+
+jest.mock('localforage');
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn(),
+  })),
+  NavigationControl: jest.fn(),
+}));
