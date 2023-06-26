35a48fe4cdf3a18e0399692694fd4a2e5d715426
Cameron Clough <cameronjclough@gmail.com>
2023-06-26 12:35:15 +0100

    rootDir

--- src/__puppeteer__/jest.config.js
+++ src/__puppeteer__/jest.config.js
@@ -4,6 +4,7 @@ delete jestConfig['testEnvironment'];
 
 module.exports = {
   ...jestConfig,
+  rootDir: '../../',
   preset: 'jest-puppeteer',
   setupFilesAfterEnv: [...jestConfig.setupFilesAfterEnv, 'expect-puppeteer'],
   testMatch: ['<rootDir>/src/__puppeteer__/**/*.test.{js,jsx,ts,tsx}'],
