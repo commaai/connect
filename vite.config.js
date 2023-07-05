7c17791c7c052158385476a2ffe7d6c07aae7928
Cameron Clough <cameronjclough@gmail.com>
2023-07-05 15:26:31 +0200

    add vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgrPlugin from 'vite-plugin-svgr';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       // TODO: compression plugin
       react(),
+      VitePWA(),
       svgrPlugin(),
       sentryPlugin,
     ].filter(Boolean),
