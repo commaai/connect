ade31778e31a86b78f40d6eab024d071f7a66d7e
Cameron Clough <cameronjclough@gmail.com>
2023-07-09 15:30:29 +0100

    improved error boundary

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -20,9 +20,21 @@ export const ArrowBackBold = (props) => (
   </SvgIcon>
 );
 
+export const Check = (props) => (
+  <SvgIcon {...props} viewBox="0 -960 960 960">
+    <path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z" />
+  </SvgIcon>
+);
+
 export const CloseBold = (props) => (
   <SvgIcon {...props} viewBox="0 -960 960 960">
     <path d="m249-183-66-66 231-231-231-231 66-66 231 231 231-231 66 66-231 231 231 231-66 66-231-231-231 231Z" />
+  </SvgIcon>
+);
+
+export const ContentCopy = (props) => (
+  <SvgIcon {...props} viewBox="0 -960 960 960">
+    <path d="M180-81q-24 0-42-18t-18-42v-603h60v603h474v60H180Zm120-120q-24 0-42-18t-18-42v-560q0-24 18-42t42-18h440q24 0 42 18t18 42v560q0 24-18 42t-42 18H300Zm0-60h440v-560H300v560Zm0 0v-560 560Z" />
   </SvgIcon>
 );
 
