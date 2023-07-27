011b5066ee1861459bd83c707c6eb1f0d26deccc
Cameron Clough <cameronjclough@gmail.com>
2023-07-27 12:17:25 +0100

    CI: add route list test

--- src/__puppeteer__/routing.test.js
+++ src/__puppeteer__/routing.test.js
@@ -18,10 +18,24 @@ describe('routing', () => {
     });
   });
 
+  it('load route list', async () => {
+    await page.goto(`http://localhost:${PORT}/4cf7a6ad03080c90`, { waitUntil: 'networkidle2' });
+    await sleep(2000);
+
+    // ".DriveList" should be visible
+    const driveList = await page.$('.DriveList');
+    expect(driveList).toBeTruthy();
+
+    // Page should have one ".DriveEntry" element
+    const driveEntries = await page.$$('.DriveEntry');
+    expect(driveEntries.length).toBe(1);
+  });
+
   it('load route from URL', async () => {
     await page.goto(`http://localhost:${PORT}/4cf7a6ad03080c90/1632948396703/1632949028503`, { waitUntil: 'networkidle2' });
     await sleep(10000);
 
+    // Should load video with src
     const video = await page.$('video');
     expect(video).toBeTruthy();
     const videoSrc = await page.evaluate((vid) => vid.getAttribute('src'), video);
