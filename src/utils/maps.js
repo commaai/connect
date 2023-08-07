1edbba068e92294922cbe83a57b86af56bb2e0fa
Kacper Rączy <gfw.kra@gmail.com>
2023-08-06 23:42:50 -0700

    Support google.com/maps links

--- src/utils/maps.js
+++ src/utils/maps.js
@@ -2,7 +2,7 @@ import { forwardLookup } from "./geocode";
 
 export async function coordinatesFromMapsUrl(urlString) {
   let url = new URL(urlString);
-  if (url.hostname === 'maps.apple.com') {
+  if (url.hostname.endsWith('maps.apple.com')) {
     if (!url.searchParams.has('ll')) {
       return null;
     }
@@ -10,19 +10,26 @@ export async function coordinatesFromMapsUrl(urlString) {
     let ll = url.searchParams.get('ll');
     let coords = ll.split(',').map((x) => parseFloat(x));
     return coords;
-  } else if (['maps.google.com', 'google.com'].includes(url.hostname)) {
-    if (!url.searchParams.has('q')) {
-      return null;
-    }
+  } else if (['maps.google.com', 'google.com'].find((h) => url.hostname.endsWith(h))) {
+    if (url.searchParams.has('q')) { // maps.google.com?q=...
+      let query = url.searchParams.get('q');
+      let places = await forwardLookup(query, null, null);
+      if (places.length === 0) {
+        return null;
+      }
+  
+      let position = places[0].position;
+      return position ? [position.lat, position.lng] : null;
+    } else { // google.com/maps/@..., google.com/maps/place/**/@...
+      let parts = url.pathname.split('/');
+      let llPart = parts.find((p) => p.startsWith('@'));
+      if (!llPart) {
+        return null;
+      }
 
-    let query = url.searchParams.get('q');
-    let places = await forwardLookup(query, null, null);
-    if (places.length === 0) {
-      return null;
+      let coords = llPart.slice(1).split(',').slice(0, 2).map((x) => parseFloat(x));
+      return coords;
     }
-
-    let position = places[0].position;
-    return [position.lat, position.lng];
   }
 
   return null;
