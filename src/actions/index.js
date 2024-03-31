9b1d854ab858c88f142555bd8638c6f8878d15b9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-03-31 11:57:01 -0700

    handle wall time jump mid-route (#452)
    * handle wrong time at start
    
    * just do this for now
    
    * just handle this one case for now

--- src/actions/index.js
+++ src/actions/index.js
@@ -286,8 +286,20 @@ export function checkRoutesData() {
       }
 
       const routes = routesData.map((r) => {
-        const startTime = r.segment_start_times[0];
-        const endTime = r.segment_end_times[r.segment_end_times.length - 1];
+        console.log(r)
+        let startTime = r.segment_start_times[0];
+        let endTime = r.segment_end_times[r.segment_end_times.length - 1];
+
+        // TODO: these will all be relative times soon
+        // fix segment boundary times for routes that have the wrong time at the start
+        if ((Math.abs(r.start_time_utc_millis - startTime) > 24*60*60*1000) &&
+            (Math.abs(r.end_time_utc_millis - endTime) < 10*1000)) {
+          console.log("fixing %s", r.fullname);
+          startTime = r.start_time_utc_millis;
+          endTime = r.end_time_utc_millis;
+          r.segment_start_times = r.segment_numbers.map((x) => startTime + (x*60*1000));
+          r.segment_end_times = r.segment_numbers.map((x) => Math.min(startTime + ((x+1)*60*1000), endTime));
+        }
         return {
           ...r,
           url: r.url.replace('chffrprivate.blob.core.windows.net', 'chffrprivate.azureedge.net'),
