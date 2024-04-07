33a93f57da856317299d255b8bffba0055226dc9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-04-06 23:19:43 -0700

    Revert "parse out log id"
    This reverts commit 417070974a6506003ea60f38fc77e6366bbed109.

--- src/url.js
+++ src/url.js
@@ -1,5 +1,4 @@
 const dongleIdRegex = /[a-f0-9]{16}/;
-const logIdRegex = /[a-f0-9-]{20}/;
 
 export function getDongleID(pathname) {
   let parts = pathname.split('/');
@@ -20,20 +19,6 @@ export function getZoom(pathname) {
     return {
       start: Number(parts[1]),
       end: Number(parts[2]),
-    };
-  }
-  return null;
-}
-
-export function getSegmentRange(pathname) {
-  let parts = pathname.split('/');
-  parts = parts.filter((m) => m.length);
-
-  if (parts.length >= 2 && logIdRegex.test(parts[1])) {
-    return {
-      lod_id: parts[1],
-      start: Number(parts[2]),
-      end: Number(parts[3]),
     };
   }
   return null;
