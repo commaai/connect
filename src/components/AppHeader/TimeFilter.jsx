e85a82eef8cffb1e79c8e83382ac7cfdd9ff025b
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2024-05-16 16:53:20 -0700

    improve TimeFilter for custom date (#463)
    * prevent invalid filter from being saved
    
    * fix wrong date being used in custom date select
    
    * set end date to 23 59 59
    
    * remove showError
    
    * indent
    
    * lint fix
    
    ---------
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- src/components/AppHeader/TimeFilter.jsx
+++ src/components/AppHeader/TimeFilter.jsx
@@ -106,7 +106,7 @@ class TimeSelect extends Component {
   changeStart(event) {
     if (event.target.valueAsDate) {
       this.setState({
-        start: event.target.valueAsDate.setHours(0, 0, 0, 0),
+        start: new Date(event.target.valueAsDate.getUTCFullYear(), event.target.valueAsDate.getUTCMonth(), event.target.valueAsDate.getUTCDate()).getTime(),
       });
     }
   }
@@ -114,7 +114,7 @@ class TimeSelect extends Component {
   changeEnd(event) {
     if (event.target.valueAsDate) {
       this.setState({
-        end: event.target.valueAsDate.setHours(23, 59, 59, 999),
+        end: new Date(event.target.valueAsDate.getUTCFullYear(), event.target.valueAsDate.getUTCMonth(), event.target.valueAsDate.getUTCDate(),23,59,59).getTime(),
       });
     }
   }
@@ -215,7 +215,7 @@ class TimeSelect extends Component {
               <input
                 label="End date"
                 type="date"
-                min={ minDate }
+                min={ startDate }
                 max={ maxDate }
                 onChange={this.changeEnd}
                 value={ endDate }
