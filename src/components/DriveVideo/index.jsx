e4e19b47081ecea467db9af3d0830694211d5979
Shane Smiskol <shane@smiskol.com>
2024-05-23 23:22:55 -0700

    clean up

--- src/components/DriveVideo/index.jsx
+++ src/components/DriveVideo/index.jsx
@@ -204,10 +204,8 @@ class DriveVideo extends Component {
     const { dispatch, isBufferingVideo, currentRoute } = this.props;
     if (!currentRoute) {
       dispatch(updateSegments());
-      console.log('updateSegments')
       // FIXME: currentRoute can't be true if it was just false?
       if (currentRoute && isBufferingVideo) {
-        console.log('buffering6!')
         dispatch(bufferVideo(false));
       }
       return;
