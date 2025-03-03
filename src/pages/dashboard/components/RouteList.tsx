c3c0c9d3d3024c32907e9d03b4bedd7b5b18540b
yuzisee <yuzisee@users.noreply.github.com>
2025-03-03 04:05:08 -0800

    The old connect.comma.ai shows the frames of video in the RouteList, which has the benefit that you can quickly see which were at daytime vs. nighttime based purely on the color themes of the frames.  Without them you have to manually read every time of day line and it's a slower (and more effort) to read line-by-line to scroll down to a certain moment you had in mind
    Loading all these frames increases server load though, so in the new connect we may as well color code the background of each <a> with some sort of "sunlight" color temperature based on latitude & longitude and time of day instead.
    
    Original discussion here: https://discord.com/channels/469524606043160576/1244366765983535145/1345905314989342730

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -10,6 +10,8 @@ import {
   type VoidComponent,
 } from 'solid-js'
 import dayjs from 'dayjs'
+import SunCalc from 'suncalc'
+import kelvinToRgb from 'kelvin-to-rgb'
 
 import { fetcher } from '~/api'
 import Card, { CardContent, CardHeader } from '~/components/material/Card'
@@ -26,21 +28,80 @@ const RouteCard: VoidComponent<RouteCardProps> = (props) => {
   const startTime = () => dayjs(props.route.start_time_utc_millis)
   const endTime = () => dayjs(props.route.end_time_utc_millis)
 
-  return (
-    <Card
-      class="max-w-none"
-      href={`/${props.route.dongle_id}/${props.route.fullname.slice(17)}`}
-      activeClass="md:before:bg-primary"
-    >
-      <CardHeader
-        headline={startTime().format('ddd, MMM D, YYYY')}
-        subhead={`${startTime().format('h:mm A')} to ${endTime().format('h:mm A')}`}
-      />
+  const dayNightFraction = () => {
+    const times = SunCalc.getTimes(endTime(), props.route.end_lat, props.route.end_lng);
+    const now_ts = endTime().valueOf();
 
-      <CardContent>
-        <RouteStatistics route={props.route} />
-      </CardContent>
-    </Card>
+    if (times.goldenHourEnd.valueOf() < now_ts && now_ts < times.goldenHour.valueOf()) {
+      // "Daytime"
+      return Number.POSITIVE_INFINITY;
+    } else if (times.nightEnd.valueOf() <= now_ts && now_ts <= times.night.valueOf()) {
+      // The idea here is to color code with some sort of "sunlight" color temperature, based on latitude & longitude and time of day
+      var percentage = 1.0;
+
+      // Temperature calculation from https://github.com/claytonjn/hass-circadian_lighting/blob/2f87bdaf86c602983c054b3f6fcd51a5198c1c20/custom_components/circadian_lighting/__init__.py#L295 instead
+      if(now_ts < times.solarNoon.valueOf()) {
+        percentage = 1.0 - Math.pow((times.solarNoon.valueOf() - now_ts)/(times.solarNoon.valueOf() - times.nightEnd.valueOf()), 2);
+      }
+      if(now_ts > times.solarNoon.valueOf()) {
+        percentage = 1.0 - Math.pow((now_ts - times.solarNoon.valueOf())/(times.night.valueOf() - times.solarNoon.valueOf()), 2);
+      }
+
+      // INVARIANT: If you get here, `percentage` is between 0.0 and 1.0
+      return percentage;
+    } else {
+      // "Nighttime"
+      return null;
+    }
+
+  };
+
+  const sunlightBorderStyle = () => {
+    const percentage = dayNightFraction();
+
+    if (percentage === null) {
+      // No border at night. (There's no sun at night, anyway.)
+      return '0px none transparent';
+    } else {
+      const max_w = Math.PI / 2;
+      const w = Math.sin(SunCalc.getPosition(endTime(), props.route.end_lat, props.route.end_lng).altitude) / max_w;
+      const sunBorderPx = 1 * (1 - w) + 3 * (w); // interpolate from 1px to 3px
+
+      var sunColorRgb;
+      if (0.0 <= percentage && percentage <= 1.0) {
+        const k = 2500 * (1 - percentage) + 5500 * (percentage); // interpolate from 2500K to 5500K
+        sunColorRgb = kelvinToRgb(k);
+      } else {
+        // If you want to actually calculate sky blue, you'll need something like https://github.com/elonen/js-sky-sim/blob/7e45f96a7842a1f8fc5a495d6bb15e914c54b0fb/sky-sim.html#L105
+        // Or if you want something trivially simple instead, try https://github.com/x8BitRain/meta-theme-sky-color
+        sunColorRgb = kelvinToRgb(40000);
+      }
+
+      return sunBorderPx + 'px solid rgb(' + sunColorRgb[0] + ',' + sunColorRgb[1] + ',' + sunColorRgb[2] + ')';
+    }
+
+  };
+
+  return (
+    <div
+      style:border-radius="1.5ex"
+      style:border={sunlightBorderStyle()}
+    >
+      <Card
+        class="max-w-none"
+        href={`/${props.route.dongle_id}/${props.route.fullname.slice(17)}`}
+        activeClass="md:before:bg-primary"
+      >
+        <CardHeader
+          headline={startTime().format('ddd, MMM D, YYYY')}
+          subhead={`${startTime().format('h:mm A')} to ${endTime().format('h:mm A')}`}
+        />
+
+        <CardContent>
+          <RouteStatistics route={props.route} />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
