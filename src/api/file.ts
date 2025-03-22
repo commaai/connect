ba6c4c0190adfd4586c27fbd6de401ff23098ccd
Trey Moen <trey@moen.ai>
2025-03-21 18:56:18 -0700

    clean up, add type

--- src/api/file.ts
+++ src/api/file.ts
@@ -1,7 +1,9 @@
 import { Files, Route, UploadFileMetadataResponse } from "~/types";
 import { fetcher } from ".";
 
-export const getFiles = (routeName: Route["fullname"]): Promise<Files> => fetcher<Files>(`/v1/route/${routeName}/files`);
+export const getAlreadyUploadedFiles = (routeName: Route["fullname"]): Promise<Files> => 
+  fetcher<Files>(`/v1/route/${routeName}/files`);
+
 export const requestToUploadFiles = (dongleId: string, paths: string[], expiryDays: number = 7) =>
   fetcher<UploadFileMetadataResponse>(`/v1/${dongleId}/upload_urls/`, {
     method: "POST",
