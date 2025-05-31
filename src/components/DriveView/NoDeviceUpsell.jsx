578ea87fe911d2455aa3965353f41db3e4321c52
Nelson Chen <crazysim@gmail.com>
2025-05-30 20:06:51 -0700

    Note Pair Device in Settings in NoDeviceUpsell
    Modern OP/Forks isn't so upfront with showing the QR Code in the home screen. Note it might be in settings.

--- src/components/DriveView/NoDeviceUpsell.jsx
+++ src/components/DriveView/NoDeviceUpsell.jsx
@@ -13,6 +13,7 @@ const NoDeviceUpsell = () => (
       <ul className="my-0">
         <li>Your device is connected to the internet</li>
         <li>You have installed the latest version of openpilot</li>
+        <li>You may need to look for "Pair Device" in Settings</li>
       </ul>
       <p>
         If you still cannot see a QR code, your device may already be paired to
