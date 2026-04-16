4b1ad9e951d07b1368e7d11cb10a863599c1d150
stefpi <19478336+stefpi@users.noreply.github.com>
2026-04-15 18:22:20 -0700

    remove vite ssl package

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,6 @@ export default defineConfig(({ mode }) => {
       }),
       svgrPlugin(),
       sentryPlugin,
-      // basicSsl(),
     ].filter(Boolean),
     optimizeDeps: {
       esbuildOptions: {
