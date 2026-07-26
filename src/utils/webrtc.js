3e0381bfacd864e87509ea7791545d893c53b66f
stefpi <19478336+stefpi@users.noreply.github.com>
2026-07-26 12:50:55 -0700

    change to relay candidate shortcut

--- src/utils/webrtc.js
+++ src/utils/webrtc.js
@@ -172,6 +172,10 @@ export class WebRTCConnection extends EventTarget {
           if (!evt.candidate) {
             this._log('ICE gathering complete');
             resolve();
+          } else if (['relay'].includes(evt.candidate.type)) {
+            // short cut when at least the relay candidate is added
+            this._log(`Using ${evt.candidate.type} candidate`, evt.candidate);
+            resolve();
           }
         });
       });
