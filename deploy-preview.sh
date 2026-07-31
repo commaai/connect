83cad085de1794f05c10efe645d644374583d2ad
stefpi <19478336+stefpi@users.noreply.github.com>
2026-07-30 21:22:41 -0700

    generate gallery report in ci

--- deploy-preview.sh
+++ deploy-preview.sh
@@ -4,7 +4,10 @@ set -e
 BRANCH="${1:-$(git branch --show-current)}"
 
 PREVIEW=1 bun build:production
+bun run build:gallery
+cp dist-gallery/connect-gallery.html dist/connect-gallery.html
 bunx wrangler pages deploy dist --project-name=connect --branch="$BRANCH"
 
 echo ""
 echo "Preview URL: https://${BRANCH}.connect-d5y.pages.dev"
+echo "Gallery report: https://${BRANCH}.connect-d5y.pages.dev/connect-gallery.html"
