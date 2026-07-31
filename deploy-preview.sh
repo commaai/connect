0ce20a46cfc9f58c4681c64283bfe74972348961
stefpi <19478336+stefpi@users.noreply.github.com>
2026-07-31 14:50:50 -0700

    simplify

--- deploy-preview.sh
+++ deploy-preview.sh
@@ -4,7 +4,6 @@ set -e
 BRANCH="${1:-$(git branch --show-current)}"
 export PREVIEW=1
 
-bun install --cwd scripts --frozen-lockfile
 bun run build:gallery -- --output dist --baseline-url https://latest.connect-d5y.pages.dev
 bunx wrangler pages deploy dist --project-name=connect --branch="$BRANCH"
 
