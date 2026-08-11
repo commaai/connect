f4b74e991043425634c72f0ddc230e9613d49ab2
stefpi <19478336+stefpi@users.noreply.github.com>
2026-08-11 15:55:05 -0700

    fix ci

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
     name: 'chromium',
     use: {
       ...devices['Desktop Chrome'],
-      executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+      launchOptions: {
+        executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+      },
     },
   }],
   webServer: {
