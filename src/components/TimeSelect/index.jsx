2e0d8be7fd84c1c3d4e06e2d287f0aee7d49e0b5
bhavya <bsg@somaiya.edu>
2026-09-04 11:34:24 -0700

    fix stale comment

--- src/components/TimeSelect/index.jsx
+++ src/components/TimeSelect/index.jsx
@@ -44,7 +44,7 @@ const styles = (theme) => ({
   },
 });
 
-const LOOKBACK_WINDOW_MILLIS = 365 * 24 * 3600 * 1000; // 30 days
+const LOOKBACK_WINDOW_MILLIS = 365 * 24 * 3600 * 1000; // 365 days
 
 class TimeSelect extends Component {
   constructor(props) {
