885927dd2cf86b275a236894be3ed9e05700d5c6
bhavya <bsg@somaiya.edu>
2026-09-06 12:23:47 -0700

    refactor to functional style for simplicity

--- src/components/TimeSelect/index.jsx
+++ src/components/TimeSelect/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import dayjs from 'dayjs';
 
@@ -54,105 +54,83 @@ const styles = (theme) => ({
   },
 });
 
-class TimeSelect extends Component {
-  constructor(props) {
-    super(props);
+const TimeSelect = (props) => {
+  const { classes, isOpen, onClose, filter, dispatch } = props;
 
-    this.state = {
-      start: dayjs(props.filter.start).format('YYYY-MM-DD'),
-      end: dayjs(props.filter.end).format('YYYY-MM-DD'),
-    };
+  const [start, setStart] = useState(dayjs(filter.start).format('YYYY-MM-DD'));
+  const [end, setEnd] = useState(dayjs(filter.end).format('YYYY-MM-DD'));
 
-    this.changeStart = this.changeStart.bind(this);
-    this.changeEnd = this.changeEnd.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.filter !== this.props.filter) {
-      this.setState({
-        start: dayjs(this.props.filter.start).format('YYYY-MM-DD'),
-        end: dayjs(this.props.filter.end).format('YYYY-MM-DD'),
-      });
-    }
-  }
-
-  changeStart(event) {
+  const changeStart = (event) => {
     if (event.target.value) {
-      this.setState(state => ({
-        start: event.target.value,
-        end: state.end < event.target.value ? event.target.value : state.end,
-      }));
+      setStart(event.target.value);
+      setEnd(current => (
+        current < event.target.value ? event.target.value : current
+      ));
     }
-  }
+  };
 
-  changeEnd(event) {
+  const changeEnd = (event) => {
     if (event.target.value) {
-      this.setState(state => ({
-        end: event.target.value < state.start ? state.start : event.target.value,
-      }));
+      setEnd(event.target.value < start ? start : event.target.value);
     }
-  }
+  };
 
-  handleSave() {
-    const start = dayjs(this.state.start).startOf('day').valueOf();
-    const end = dayjs(this.state.end).endOf('day').valueOf();
+  const handleSave = () => {
+    dispatch(selectTimeFilter(
+      dayjs(start).startOf('day').valueOf(),
+      dayjs(end).endOf('day').valueOf(),
+    ));
+    onClose();
+  };
 
-    this.props.dispatch(selectTimeFilter(start, end));
-    this.props.onClose();
-  }
+  const minDate = dayjs().subtract(365, 'day').format('YYYY-MM-DD');
+  const maxDate = dayjs().format('YYYY-MM-DD');
 
-  render() {
-    const { classes, isOpen, onClose } = this.props;
-    const minDate = dayjs().subtract(365, 'day').format('YYYY-MM-DD');
-    const maxDate = dayjs().format('YYYY-MM-DD');
-
-    return (
-      <Modal
-        open={isOpen}
-        onClose={onClose}
-        className={classes.modalContainer}
-      >
-        <Paper className={classes.modal}>
-          <div className={classes.datePickerContainer}>
-            <div className={classes.dateField}>
-              <Typography variant="body2">Start date:</Typography>
-              <input
-                className={classes.dateInput}
-                type="date"
-                min={minDate}
-                max={maxDate}
-                onChange={this.changeStart}
-                value={this.state.start}
-              />
-            </div>
-            <div className={classes.dateField}>
-              <Typography variant="body2">End date:</Typography>
-              <input
-                className={classes.dateInput}
-                type="date"
-                min={this.state.start}
-                max={maxDate}
-                onChange={this.changeEnd}
-                value={this.state.end}
-              />
-            </div>
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      className={classes.modalContainer}
+    >
+      <Paper className={classes.modal}>
+        <div className={classes.datePickerContainer}>
+          <div className={classes.dateField}>
+            <Typography variant="body2">Start date:</Typography>
+            <input
+              className={classes.dateInput}
+              type="date"
+              min={minDate}
+              max={maxDate}
+              onChange={changeStart}
+              value={start}
+            />
           </div>
-          <Divider />
-          <div className={classes.buttonGroup}>
-            <Button variant="contained" className={classes.cancelButton} onClick={onClose}>
-              Cancel
-            </Button>
-            &nbsp;
-            <Button variant="contained" className={classes.saveButton} onClick={this.handleSave}>
-              Save
-            </Button>
+          <div className={classes.dateField}>
+            <Typography variant="body2">End date:</Typography>
+            <input
+              className={classes.dateInput}
+              type="date"
+              min={start}
+              max={maxDate}
+              onChange={changeEnd}
+              value={end}
+            />
           </div>
-        </Paper>
-      </Modal>
-    );
-  }
-}
+        </div>
+        <Divider />
+        <div className={classes.buttonGroup}>
+          <Button variant="contained" className={classes.cancelButton} onClick={onClose}>
+            Cancel
+          </Button>
+          &nbsp;
+          <Button variant="contained" className={classes.saveButton} onClick={handleSave}>
+            Save
+          </Button>
+        </div>
+      </Paper>
+    </Modal>
+  );
+};
 
 const stateToProps = (state) => ({
   filter: state.filter,
