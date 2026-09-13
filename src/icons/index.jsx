d71d4c443b64ebd340b8832b44b6d4f5624ab493
bhavya <bsg@somaiya.edu>
2026-09-13 00:47:01 -0700

    keep that same style as BatteryChargingFull

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -251,8 +251,8 @@ export const KeyboardBackspaceIcon = (props) => (
 );
 
 export const BatteryFull = (props) => (
-  <SvgIcon {...props} viewBox="0 -960 960 960">
-    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Z" />
+  <SvgIcon {...props}>
+    <path d="M15.67 4H14V2h-4v2H8.33C7.6 4 7 4.6 7 5.33v15.33C7 21.4 7.6 22 8.33 22h7.33c.74 0 1.34-.6 1.34-1.33V5.33C17 4.6 16.4 4 15.67 4z" />
   </SvgIcon>
 );
 
