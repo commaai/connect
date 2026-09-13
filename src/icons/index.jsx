77d431415c33b8c81eacbe5f7e8f5f04954b2990
bhavya <bsg@somaiya.edu>
2026-09-12 23:33:05 -0700

    add local FilterList icon

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -183,3 +183,9 @@ export const RightArrow = (props) => (
     {chevronPath(180)}
   </SvgIcon>
 );
+
+export const FilterList = (props) => (
+  <SvgIcon {...props}>
+    <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" />
+  </SvgIcon>
+);
