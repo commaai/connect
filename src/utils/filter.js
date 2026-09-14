e0d91c45cef2ea8d156e99ddcdc9fd7d2d5ef26f
stefpi <19478336+stefpi@users.noreply.github.com>
2026-09-13 18:58:39 -0700

    filter: change default to one year

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,10 @@
-const FIVE_YEARS = 5 * 365 * 24 * 60 * 60 * 1000;
+const ONE_YEAR = 365 * 24 * 60 * 60 * 1000;
 
 export function getDefaultFilter() {
   const end = new Date().setMinutes(60, 0, 0); // next hour
 
   return {
-    start: end - FIVE_YEARS,
+    start: end - ONE_YEAR,
     end
   };
 }
